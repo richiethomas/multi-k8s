c06703ccc772c8cdbd7fb03ddb3daa4a19aaebd0
Richie Thomas <rickthomas1980@gmail.com>
2023-05-31 09:04:51 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <div className="App">
         <header>
-          <h1>Fib Calculator</h1>
+          <h1>Fib Calculator v2.0</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
